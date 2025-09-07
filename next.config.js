3895e3a978748e74f27ae22578d69a091870703f
castigadorsandrick@gmail.com <castigadorsandrick@gmail.com>
2025-09-07 22:56:16 +0800

    Fix next.config.js and remove package-lock.json

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ import "./src/env.js";
 const config = {
     typescript: {
         // @ts-ignore
-        ignoresBuildErrors: true,
+        ignoreBuildErrors: true,
     },
     eslint: {
         ignoreDuringBuilds: true,
